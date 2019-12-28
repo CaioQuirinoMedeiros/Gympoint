5a55b5908318b0f3580519fa9c6f586ad193b490
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-12-28 02:31:40 -0300

    initializing api structure: - express - eslint & prettier

---
+++ gympoint-api/src/app.js
@@ -0,0 +1,32 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+    this.exceptionHandler();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+    this.server.use(cors());
+  }
+
+  routes() {}
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        console.log(err);
+      }
+
+      return res.status(500).json({ error: 'Erro interno do servidor' });
+    });
+  }
+}
+
+export default new App().server;
