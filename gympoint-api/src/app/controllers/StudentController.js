e1b18ea5fba95265ad4cd9de9064e0132c18ebb8
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-12-29 22:01:57 -0300

    list, update and delete student features

--- gympoint-api/src/app/controllers/StudentController.js
+++ gympoint-api/src/app/controllers/StudentController.js
@@ -17,6 +17,62 @@ class StudentController {
       return res.status(400).send({ error: 'Erro ao criar aluno' });
     }
   }
+
+  async index(req, res) {
+    try {
+      const students = await Student.findAll();
+
+      return res.status(200).send(students);
+    } catch (err) {
+      console.error(err);
+      return res.status(400).send({ error: 'Erro ao buscar alunos' });
+    }
+  }
+
+  async update(req, res) {
+    const { student_id } = req.params;
+    try {
+      const student = await Student.findByPk(student_id);
+
+      if (!student) {
+        return res
+          .status(404)
+          .send({ error: `O aluno ${student_id} não foi encontrado` });
+      }
+
+      await student.update(req.body)
+
+      return res.status(200).send(student);
+    } catch (err) {
+      console.error(err);
+      return res.status(400).send({ error: 'Erro ao buscar aluno' });
+    }
+  }
+
+  async destroy(req, res) {
+    const { student_id } = req.params;
+    console.log('PARAAAAAAAAMS')
+    console.log(student_id)
+
+    try {
+      const student = await Student.findByPk(student_id);
+      console.log('studeeeeeeeeeeeeeeeent')
+      console.log(student)
+
+      if (!student) {
+        return res
+          .status(404)
+          .send({ error: `O aluno ${student_id} não foi encontrado` });
+      }
+
+      await student.destroy()
+
+      return res.status(200).send();
+    } catch (err) {
+      console.error(err);
+      return res.status(400).send({ error: 'Erro ao deletar aluno' });
+    }
+  }
 }
 
 export default new StudentController();
--- gympoint-api/src/routes/student.js
+++ gympoint-api/src/routes/student.js
@@ -7,5 +7,8 @@ import validateStudentStore from '../app/validators/StudentStore';
 const routes = new Router();
 
 routes.post('/students', validateStudentStore, StudentController.store);
+routes.get('/students', StudentController.index);
+routes.put('/students/:student_id', StudentController.update);
+routes.delete('/students/:student_id', StudentController.destroy);
 
 export default routes;
